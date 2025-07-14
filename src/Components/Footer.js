cab0023bcc4fbca27f986dbedaee13307b025e7c
ivan darmawan <ivandarmawan2020@gmail.com>
2025-07-14 23:15:14 +0700

    main

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
           target="_blank"
           rel="noreferrer noopener"
         >
-          Ivan Darmawann
+          Ivan Darmawan
         </a>{" "}
         with
         <span className="text-gradient font-medium"> Love</span> &
